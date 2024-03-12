15dc24ad341f1e909955d41f5c476bb91f3fa4ea
Mahmood Naderan <nt_mahmood@yahoo.com>
2024-03-12 13:53:54 +0100

    Update addition.cu

--- addition.cu
+++ addition.cu
@@ -7,6 +7,7 @@
 #include <vector>
 #include <cassert>
 #include <string>
+#include <iomanip>
 #include "cuda_runtime_api.h"
 #include "helper.h"
 
@@ -20,20 +21,6 @@ void readInputFiles(vector<float> &v)
   float a;
   while (f >> a)
     v.push_back(a);
-}
-
-// This helper function prints the final results on the screen
-void print(vector<float> &v)
-{
-  for (auto x:v)
-    cout << x << " ";
-  cout << "\n";
-}
-void print(vector<int> &v)
-{
-  for (auto x:v)
-    cout << x << " ";
-  cout << "\n";
 }
 
 
@@ -53,6 +40,7 @@ void transferResults(float *&dv, vector<float> &hv, int n)
   //print(hv);
 }
 
+
 // This function frees device memory
 void freeMemory(float *&dv)
 {
@@ -60,25 +48,23 @@ void freeMemory(float *&dv)
 }
 
 
-// This function returns the SM number
-__device__ uint get_smid(void) {
-     uint ret;
-     asm("mov.u32 %0, %smid;" : "=r"(ret) );
-     return ret;
-}
-
 
 // Kernel inplementation
 __global__ void simpleAdd(float *v, int n)
 {
   int i = blockIdx.x * blockDim.x + threadIdx.x;
   if (i < n) {
-    printf("SM(%d) Block(%d,%d,%d) Thread(%d,%d,%d) -> %.2f + 1\n", get_smid(), blockIdx.x, blockIdx.y, blockIdx.z, threadIdx.x, threadIdx.y, threadIdx.z, v[i]);
+    int s = get_smid();
+    atomicAdd(&g_smid[s], 1); //Atomic add is needed
+    printf("SM(%d) | Block(%d,%d,%d) | Thread(%d,%d,%d) | TID(%d) -> %.2f + 1\n", 
+            s, blockIdx.x, blockIdx.y, blockIdx.z, threadIdx.x, threadIdx.y, threadIdx.z, i, v[i]);
     v[i] = v[i] + 1;
   }
 }
 
-    
+
+
+
 int main(int argc, char** argv)
 {
   if (argc != 2) {
@@ -86,7 +72,8 @@ int main(int argc, char** argv)
     return 1;
   }
   int blockSize = stoi(argv[1]);
-  printInfo(blockSize);
+  int smCount;
+  printInfo(smCount, blockSize);
   
   vector<float> hostVector;
   readInputFiles(hostVector);
@@ -97,7 +84,7 @@ int main(int argc, char** argv)
   allocateOnDevice(deviceVector, hostVector, n);
 
   int numBlocks = (n + blockSize - 1) / blockSize;
-  
+
   cudaEvent_t start, stop;
   float elapsedTime;
   cudaEventCreate(&start); cudaEventRecord(start,0);
@@ -111,11 +98,13 @@ int main(int argc, char** argv)
   cout << "\naddABC elapsed time : " << elapsedTime << " ms\n";
   cudaDeviceSynchronize();
 
+  printSM(smCount);
+
   transferResults(deviceVector, hostVector, n);
   freeMemory(deviceVector);
   
-  cout << "\nResult:\n";
-  print(hostVector);
+  /*cout << "\nResult:\n";
+  print(hostVector);*/
 
   cout << "Done\n";
   return 0;
